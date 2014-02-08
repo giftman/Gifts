29150aed70dbf92cde59ca1b2162c993e81568e6
root <root@xzr.mongo>
2014-02-08 12:00:33 -0500

    rename--

--- Lua/moveit/table.lua
+++
@@ -1,8 +0,0 @@
---local count = 0
---function Entry (b) count = count + 1 end
---dofile("data")
---print("number of entries: ".. count)
-local authors = {}
-function Entry(b) authors[b.author] = true end
-dofile("data")
-for name in pairs(authors) do print(name) end
