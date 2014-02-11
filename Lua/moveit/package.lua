53d89c9a50dce69acddd231139b4697625959df9
giftman <121552591@qq.com>
2014-02-11 11:15:05 -0500

    package 4 : with privacy

--- Lua/moveit/package.lua
+++ Lua/moveit/package.lua
@@ -1,6 +1,11 @@
 local P = {}
 complex = P
-P.i = {r=0,i=1}
+local function checkComplex (c)
+    if not ((type(c)== "table") and
+        tonumber(c.r) and tonumber(c.i)) then 
+        error("bad complex number",3)
+    end
+end
 function P.new(r,i) return {r=r,i=i} end
 function P.add(c1,c2)
     return P.new(c1.r + c2.r, c1.i + c2.i)
