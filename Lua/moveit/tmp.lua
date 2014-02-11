9056f836d70a4fe799971b361716dac9486a2ad9
giftman <121552591@qq.com>
2014-02-11 05:09:16 -0500

    14.1 obmit..

--- Lua/moveit/tmp.lua
+++ Lua/moveit/tmp.lua
@@ -1,1 +1,12 @@
-for n in pairs(_G) do print(n) end
+--14.1 what ..it means ...
+function getfield (f)
+    local v = _G
+    for w in string.gfind(f,"[%w_]+") do
+        v = v[w]
+    end
+    return v
+end
+
+a.b.c.d.e = v
+local temp = a.b.c.d
+temp.e = v
