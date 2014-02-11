2eb0d559b97648ef22251bd83f57f105105c61d1
giftman <121552591@qq.com>
2014-02-11 13:51:04 -0500

    package 5: the better one

--- Lua/moveit/package.lua
+++ Lua/moveit/package.lua
@@ -1,26 +1,33 @@
-local P = {}
-complex = P
+--easy to change private or not ,and don't need to call with a table name
 local function checkComplex (c)
     if not ((type(c)== "table") and
         tonumber(c.r) and tonumber(c.i)) then 
         error("bad complex number",3)
     end
 end
-function P.new(r,i) return {r=r,i=i} end
-function P.add(c1,c2)
-    return P.new(c1.r + c2.r, c1.i + c2.i)
+local function new(r,i) return {r=r,i=i} end
+local function add(c1,c2)
+    checkComplex(c1)
+    checkComplex(c2)
+    return new(c1.r + c2.r, c1.i + c2.i)
 end
-function P.sub (c1,c2)
-    return P.new(c1.r - c2.r, c1.i - c2.i)
-end
-
-function P.mul (c1,c2)
-    return P.new(c1.r*c2.r - c1.i*c2.i,c1.r*c2.i + c1.i*c2.r)
+local function sub (c1,c2)
+    return new(c1.r - c2.r, c1.i - c2.i)
 end
 
-function P.inv (c)
+local function mul (c1,c2)
+    return new(c1.r*c2.r - c1.i*c2.i,c1.r*c2.i + c1.i*c2.r)
+end
+
+local function inv (c)
     local n = c.r^2 + c.i^2
-    return P.new(c.r/n, -c.i/n)
+    return new(c.r/n, -c.i/n)
 end
 
-return P
+complex = {
+    new = new,
+    add = add,
+    sub = sub,
+    mul = mul,
+    div = div,
+}
