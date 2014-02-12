813472a9fae692bdff86ea4ba983b56ad0bdc426
giftman <121552591@qq.com>
2014-02-12 09:56:17 -0500

    object 1

--- Lua/moveit/object.lua
+++ Lua/moveit/object.lua
@@ -1,23 +1,15 @@
---table vs object ?
-Account = {balance = 0}
---[[
-function Account.withdraw (v)
-    Account.balance = Account.balance -v
+--a simple object and for use 
+Account = {
+    balance = 0,
+    withdraw = function(self,v)
+        self.balance = self.balance -v 
+    end
+}
+
+function Account:deposit (v)
+    self.balance = self.balance + v
 end
 
---Account.withdraw(100.00)
-a = Account;
-Account = nil
-a.withdraw(100.00)
+Account.withdraw(Account,1000.00)
+Account:deposit(100.00)
 print(Account.balance)
-]]
---up methods use a global value "Account" in its own...its not a good idea.
-function Account.withdraw(self,v)
-    self.balance = self.balance -v
-end
-
-a = Account;
-Account = nil
-a.withdraw(a,100.00)
-print(a.balance)
-
