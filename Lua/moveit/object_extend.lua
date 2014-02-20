a4d91bfd5ab833eab80120b5114ffb1b36cc7cfb
giftman <121552591@qq.com>
2014-02-20 13:47:50 -0500

    object

---
+++ Lua/moveit/object_extend.lua
@@ -0,0 +1,36 @@
+Account = {balance = 0}
+
+function Account:new (o)
+    o = o or {}
+    setmetatable(o,self)
+    self.__index = self
+    return o
+end
+
+function Account:deposit (v)
+    self.balance = self.balance + v
+    print "Account deposit \n"
+end
+
+function Account:withdraw (v)
+    if v >self.balance then error "insufficient funds" end
+    self.balance = self.balance - v
+end
+
+SpecialAccount = Account:new()
+s = SpecialAccount:new {limit = 1000.00}
+function SpecialAccount:getLimit ()
+    return self.limit or 0
+end
+function SpecialAccount:withdraw (v)
+    if v-self.balance >= self:getLimit() then
+        error "insufficient funds"
+    end
+    self.balance = self.balance -v
+end
+
+
+s:deposit(100.00)
+print(s.balance)
+print(s.getLimit())
+
