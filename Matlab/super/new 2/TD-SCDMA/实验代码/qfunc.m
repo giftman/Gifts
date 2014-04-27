404c6ea687fb36cceee82112ab9e546c8f5555ee
giftman <121552591@qq.com>
2014-04-27 17:14:33 +0800

    mv many big file to BaiduYun,only code and note can be in github

--- Matlab/super/new 2/TD-SCDMA/实验代码/qfunc.m
+++
@@ -1,5 +0,0 @@
-
-
-function y=qfunc(x)
-%******¸ßË¹Qº¯Êý********
-y=(1/2)*erfc(x/sqrt(2));
--- Matlab/super/yong/code/Untitled.m
+++
@@ -1,7 +0,0 @@
-f=imread('ImageEncrypted.tif');
-imshow(f);
-h=imhist(f);
-plot(h)
-axis([0 255 0 700])
-set(gca,'xtick',[0:50:255])
-set(gca,'ytick',[0:100:700])
