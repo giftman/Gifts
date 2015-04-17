09d39642cdfe498860f7da45892682fc105e72ad
winFan <winfan@winFans-Mac-mini.local>
2015-04-17 14:35:42 +0800

    eidit apk version 1.0   resign the apk && change icon and union_background

---
+++ Python/moveit/tornado/editapk/gl.py
@@ -0,0 +1,19 @@
+#gl.py
+xmlkey=["isShowTestView","developVersion","productVersion","publishDate","publishDateQuickFight","IS_DEBUG","IS_LOG","QD_Key","QD_Name","QD_Code1","QD_Code2","QD_Use_Logo","QD_Use_Mango_Logo","Partition_Key","Login_Setting_Url","Login_Domain","Backup_Login_Setting_Url","Help_File_Url","Login_Type","isOffical","zone","Gift_Acitvity_Get_Url","Log_Start_Url","Gift_Activity_Name","Gift_Activity_Url","customer_server","gameName"]
+apkName=""
+apk_directory="tmp"
+config_str=apk_directory+"/assets/raw/config.xml"
+inputSdk="/Volumes/cbzr/SDK/"
+output="signed_apk/"
+clound="/Volumes/cbzr/SDK_sign/"
+
+
+keystorestr="data/cbzr.keystore"
+storepass="mango123"
+keypass="mango123"
+cmd = "jarsigner -keystore {keystorestr} -storepass {storepass} -keypass {keypass} -digestalg SHA1 -sigalg MD5withRSA -signedjar {signedApkFile} {unsignedApkFile} cbzr.keystore"
+cmd = cmd.replace("{keystorestr}",keystorestr)
+cmd = cmd.replace("{storepass}",storepass)
+cmd = cmd.replace("{keypass}",keypass)
+pngkeys=["icon.png","union_background.jpg"]
+replaceFiles=["assets/art/auth/union_background.jpg","res/drawable-hdpi/icon.png","res/drawable-mdpi/icon.png","res/drawable-ldpi/icon.png","assets/raw/config.xml"]
--- Python/moveit/tornado/editapk/tornado_test.py
+++ Python/moveit/tornado/editapk/tornado_test.py
@@ -1,8 +1,19 @@
+#coding:utf-8
 import tornado.ioloop
+import tornado.httpserver
 import tornado.web
 import zipfile
 import os
 import shutil
+from shutil import copy
+import gl
+from xml.dom import minidom
+import fnmatch
+ 
+def iterfindfiles(path, fnexp):
+	for root, dirs, files in os.walk(path):
+		for filename in fnmatch.filter(files, fnexp):
+			yield os.path.join(root, filename)
 
 def zip_dir(dirname,zipfilename):
 	filelist=[]
@@ -12,7 +23,7 @@ def zip_dir(dirname,zipfilename):
 		for root,dirs,files in os.walk(dirname):
 			for name in files:
 				filelist.append(os.path.join(root,name))
-	f = zipfile.ZipFile(zipfilename,'w')
+	f = zipfile.ZipFile(zipfilename,'w',zipfile.ZIP_DEFLATED)
 	for tar in filelist:
 		arcname = tar[len(dirname):]
 		print arcname
@@ -21,41 +32,151 @@ def zip_dir(dirname,zipfilename):
 
 def unzip_apk(apkfilename):
 	f = zipfile.ZipFile(apkfilename)
-	file_name = apkfilename[:-4]
-	f.extractall(file_name)
+	gl.apkName= os.path.basename(apkfilename)
+	file_name = gl.apkName[:-4]
+	print file_name
+	gl.apk_directory="static/tmp/"+ file_name
+	print gl.apk_directory
+	gl.config_str=gl.apk_directory+"/assets/raw/config.xml"
+	del_folder(gl.apk_directory)
+	# f.extractall(gl.apk_directory)
+	for replaceFile in gl.replaceFiles:
+		f.extract(replaceFile,gl.apk_directory)
 	f.close()
+	copy(gl.apk_directory + "/res/drawable-hdpi/icon.png","static/images/")
+	copy(gl.apk_directory + "/assets/art/auth/union_background.jpg","static/images/")
 
+def get_attrvalue(node,attrname):
+	return node.getAttribute(attrname) if node else ''
+
+def get_nodevalue(node,index = 0):
+	return node.childNodes[index].nodeValue if node else ''
+def set_nodevalue(node,newValue,index = 0):
+	node.childNodes[index].nodeValue = newValue 
+
+def apkRemove(apkFile,removeFile):
+	rmcmd = "aapt remove {apkFile} {removeFile}"
+	rmcmd = rmcmd.replace("{apkFile}",apkFile)
+	rmcmd = rmcmd.replace("{removeFile}",removeFile)
+	print rmcmd
+	os.system(rmcmd)
+def apkAdd(apkFile,removeFile):
+	rmcmd = "aapt add {apkFile} {removeFile}"
+	rmcmd = rmcmd.replace("{apkFile}",apkFile)
+	rmcmd = rmcmd.replace("{removeFile}",removeFile)
+	print rmcmd
+	os.system(rmcmd)
+def signApk(apkFile,outputFile):
+	cmdshell =gl.cmd
+	cmdshell = cmdshell.replace("{unsignedApkFile}",apkFile)
+	cmdshell = cmdshell.replace("{signedApkFile}",outputFile)
+	print "执行签名"
+	os.system(cmdshell)
+def zipAlign(apkFile,outputFile):
+	cmdshell ="zipalign -f 4 {signedApkFile} {zipalignApkFile}"
+	cmdshell = cmdshell.replace("{signedApkFile}",apkFile)
+	cmdshell = cmdshell.replace("{zipalignApkFile}",outputFile)
+	print "执行zipalign"
+	os.system(cmdshell)
+
+def get_xmlnode(node,name):
+	return node.getElementsByTagName(name) if node else []
+
+def xml_to_string(doc):
+	return doc.toxml('UTF-8')
+def xml_write_to_file(dom,filestr):
+	with open(filestr,'w') as f:
+		f.write(xml_to_string(dom))
+def parse_xml(xmlstr):
+	configData = {}
+	doc = minidom.parse(xmlstr)
+	root = doc.documentElement
+	for each_key in gl.xmlkey:
+		user_nodes = get_xmlnode(root,each_key)
+		configData[each_key] = get_nodevalue(user_nodes[0]).encode('utf-8','ignore')
+	return configData
+def zip_file_and_sign():
+	apkstr = gl.apk_directory + "/" +gl.apkName
+	copy(gl.inputSdk + gl.apkName,apkstr)
+	for replaceFile in gl.replaceFiles:
+		apkRemove(apkstr,replaceFile)
+		
+	apkRemove(apkstr,"META-INF/CERT.RSA")
+	apkRemove(apkstr,"META-INF/CERT.SF")
+	apkRemove(apkstr,"META-INF/MANIFEST.MF")
+
+	presentStr = os.getcwd()
+	os.chdir(gl.apk_directory)
+	for replaceFile in gl.replaceFiles:
+		apkAdd(gl.apkName,replaceFile)
+	os.chdir(presentStr)
+
+	signApk(apkstr,gl.output + "sign_" +gl.apkName)
+	zipAlign(gl.output + "sign_" +gl.apkName,apkstr)
+	copy(apkstr,gl.clound + "sign_" + gl.apkName)
+	del_folder(gl.apk_directory)
+def del_folder(fileName):
+	if os.path.exists(fileName):
+		shutil.rmtree(fileName)
+		print "rm " + fileName
+
+class UploadHandler(tornado.web.RequestHandler):
+	def get(self):
+		import glob
+		names = glob.glob(gl.inputSdk +"*.apk")
+		self.render('choosefile.html',names=names)
 class MainHandler(tornado.web.RequestHandler):
 	def get(self):
-		filename = os.path.join(os.path.dirname(__file__),"cbzz_uc.apk")
-		print filename
+		fileid = self.get_argument("id")
+		filename = os.path.join(fileid)
 		unzip_apk(filename)
-		self.render('index.html')
-class WrapHandler(tornado.web.RequestHandler):
-	def map_by_first_letters(self,text):
-		mapped = dict()
-		for line in text.split('\r\n'):
-			for word in [x for x in line.split(' ') if len(x) > 0]:
-				if word[0] not in mapped: mapped[word[0]] =[]
-				mapped[word[0]].append(word)
-		return mapped
-
+		configData = parse_xml(gl.config_str)
+		self.render('index.html',configData=configData,xmlkey=gl.xmlkey)
 	def post(self):
-		source_text=self.get_argument("source")
-		text_to_change=self.get_argument("change")
-		source_map=self.map_by_first_letters(source_text)
-		change_lines=text_to_change.split('\r\n')
-		self.render('munged.html',source_map=source_map,change_lines=change_lines,choice=random.choice)
+	#文件的暂存路径
+		upload_path=os.path.join(os.path.dirname(__file__),'static/images')  
+		for pngkey in gl.pngkeys:
+	        	#提取表单中‘name’为‘file’的文件元数据
+			file_metas=self.request.files[pngkey[:-4]]    
+			filepath=""
+	        	for meta in file_metas:
+	        		filename=meta['filename']
+	        		print filename
+	        		filepath=os.path.join(upload_path,pngkey)
+ 	         		#有些文件需要已二进制的形式存储，实际中可以更改
+	        		with open(filepath,'wb') as up:      
+					up.write(meta['body'])
+			for filename in iterfindfiles(gl.apk_directory,pngkey):
+				print filename
+				copy(filepath,filename)
+				print "copy:"+ filepath + "to:" +filename
+		configData = parse_xml(gl.config_str)
+		self.render('index.html',configData=configData,xmlkey=gl.xmlkey)
+class WrapHandler(tornado.web.RequestHandler):
+	def post(self):
+		doc = minidom.parse(gl.config_str)
+		root = doc.documentElement
+		for each_key in gl.xmlkey:
+			user_nodes = get_xmlnode(root,each_key)
+			set_nodevalue(user_nodes[0],self.get_argument(each_key))
+		xml_write_to_file(doc,gl.config_str)	
+		zip_file_and_sign()
+		self.render('package_result.html')
 settings = {
-	'template_path':os.path.join(os.path.dirname(__file__),"templates"),
-	'static_path':os.path.join(os.path.dirname(__file__),"static"),
-	'debug':True,
-	}
+		'template_path':os.path.join(os.path.dirname(__file__),"templates"),
+		'static_path':os.path.join(os.path.dirname(__file__),"static"),
+		'debug':True,
+		}
 application = tornado.web.Application([
-	(r"/",MainHandler),
+	(r"/",UploadHandler),
+	(r"/config",MainHandler),
 	(r"/poem",WrapHandler),
-],**settings)
+	(r"/(.*\.png)", tornado.web.StaticFileHandler, dict(path=settings['static_path'])),
+	],**settings)
 
 if __name__ == "__main__":
-	application.listen(8888)
+	sockets = tornado.netutil.bind_sockets(8888)
+	# tornado.process.fork_processes(5)                          #不同逻辑阻塞时不同ip可以分到不同进程
+	server = tornado.httpserver.HTTPServer(application, xheaders=True)
+	server.add_sockets(sockets)
 	tornado.ioloop.IOLoop.instance().start()
